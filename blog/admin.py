41133ba3cd2e343b858c670b562f482eab5ddcbf
Maximiliano Lopez <max.lopezp@alumnos.duoc.cl>
2019-05-23 00:59:10 -0400

    jaja salu2
    benduisone

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models import Post,Cliente,Orden,Tecnico
 
+class AdminCliente(admin.ModelAdmin):
+    list_display = ["__str__","nombre"]
+    form = ClienteForm
+
+class AdminTecnico(admin.ModelAdmin):
+    list_display_tecnico = ["__str__"]
+    form = TecnicoForm
+
+class AdminOrden(admin.ModelAdmin):
+    list_display_orden = ["__str__"]
+    form = OrdenForm
+    
 admin.site.register(Post)
 admin.site.register(Cliente)
 admin.site.register(Orden)
